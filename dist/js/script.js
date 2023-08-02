65cd296245acc9ac09b3d5dc9557e6a44bddc4e6
Muhammad-Dzaki-Arrahman <kiarrahman05@gmail.com>
2023-08-02 19:46:36 +0700

    modified:   dist/js/script.js 	modified:   dist/output.css 	modified:   index.html 	modified:   package-lock.json 	modified:   package.json 	modified:   src/input.css

--- dist/js/script.js
+++ dist/js/script.js
@@ -4,15 +4,18 @@ window.onscroll = function () {
   const fixedNav = header.offsetTop;
   const namaku = document.querySelector("#namaku");
   const warnalist = document.querySelector("li");
+  const toTop = document.querySelector("#toTop");
 
   if (window.scrollY > fixedNav) {
     header.classList.add("navbar-fixed");
     namaku.classList.add("warna-nama");
     warnalist.classList.add("list-warna");
+    toTop.classList.remove("hidden");
   } else {
     header.classList.remove("navbar-fixed");
     namaku.classList.remove("warna-nama");
     warnalist.classList.remove("list-warna");
+    toTop.classList.add("hidden");
   }
 };
 
@@ -23,4 +26,12 @@ const navmenu = document.querySelector("#nav-menu");
 hamberger.addEventListener("click", function () {
   hamberger.classList.toggle("hamberger-active");
   navmenu.classList.toggle("hidden");
+});
+
+// click diluar hamberger
+window.addEventListener("click", function (e) {
+  if (e.target != hamberger && e.target != navmenu) {
+    hamberger.classList.remove("hamberger-active");
+    navmenu.classList.add("hidden");
+  }
 });
