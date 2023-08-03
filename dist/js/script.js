b64fe40094740fe2a069f7dfa043bea14b43be58
Muhammad-Dzaki-Arrahman <kiarrahman05@gmail.com>
2023-08-03 12:24:53 +0700

    modified:   dist/js/script.js 	modified:   dist/output.css 	modified:   index.html 	modified:   src/input.css

--- dist/js/script.js
+++ dist/js/script.js
@@ -3,18 +3,22 @@ window.onscroll = function () {
   const header = document.querySelector("header");
   const fixedNav = header.offsetTop;
   const namaku = document.querySelector("#namaku");
-  const warnalist = document.querySelector("li");
+  const warnalist = document.querySelectorAll("li");
   const toTop = document.querySelector("#toTop");
 
   if (window.scrollY > fixedNav) {
     header.classList.add("navbar-fixed");
     namaku.classList.add("warna-nama");
-    warnalist.classList.add("list-warna");
+    for (let i = 0; i < warnalist.length; i++) {
+      warnalist[i].classList.add("list-warna");
+    }
     toTop.classList.remove("hidden");
   } else {
     header.classList.remove("navbar-fixed");
     namaku.classList.remove("warna-nama");
-    warnalist.classList.remove("list-warna");
+    for (let i = 0; i < warnalist.length; i++) {
+      warnalist[i].classList.remove("list-warna");
+    }
     toTop.classList.add("hidden");
   }
 };
